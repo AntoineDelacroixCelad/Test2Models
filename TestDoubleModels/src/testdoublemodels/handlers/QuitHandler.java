69aca93cf5f898ec80857f98c1ae9f99331a0b47
antoine <antoine.delacroix@celad.com>
2015-11-23 14:44:20 +0100

    Ajout des spies et live editor

--- TestDoubleModels/src/testdoublemodels/handlers/QuitHandler.java
+++ TestDoubleModels/src/testdoublemodels/handlers/QuitHandler.java
@@ -12,14 +12,16 @@
 package testdoublemodels.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.IWorkbench;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 
 
 public class QuitHandler {
 	@Execute
-	public void execute(IWorkbench workbench, Shell shell){
+	public void execute(EPartService partService, IWorkbench workbench, Shell shell){
 		if (MessageDialog.openConfirm(shell, "Confirmation",
 				"Do you want to exit?")) {
 			workbench.close();
